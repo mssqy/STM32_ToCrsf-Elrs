1c3e05f732a270a59e60c1bde8b02805066f8e73
mssqy <72926804+mssqy@users.noreply.github.com>
2023-10-21 19:55:25 +0800

    Add files via upload

---
+++ MDK-ARM/RTE/_CRSF_ELRS/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'CRSF_ELRS' 
+ * Target:  'CRSF_ELRS' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
